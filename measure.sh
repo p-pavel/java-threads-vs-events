f9f154eb6dc9cbdba291cab92c8092b6d81b4866
Pavel Perikov <pavel@perikov.consulting>
2024-09-10 19:09:47 +0400

    measure.sh recovered from FreeBSD

--- measure.sh
+++ measure.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 if uname | grep Darwin > /dev/null
 then
   TOP_FLAGS="-pid"
@@ -5,38 +7,27 @@ else
   TOP_FLAGS="-H -p"
 fi
 
-collectTopInfo() {
-  local TOP_PID
-  top $TOP_FLAGS $PID > $FNAME.top &
-  TOP_PID=$!
-  sleep 5
-  kill $TOP_PID > /dev/null
-}
 
 do_measure()  {
   MODE=$1
-  PAUSE_BEFORE_PROFILING=20
+  PAUSE_BEFORE_PROFILING=120
   NUM_STREAMS=400
   FNAME="result-${MODE}_${NUM_STREAMS}"
   echo Running $MODE, streams=$NUM_STREAMS, file names $FNAME
   mkfifo $FNAME.fifo
-  java -Xss300k -XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints -jar socket_test $NUM_STREAMS $MODE > $FNAME.fifo &
+  java -Xss300k -XX:+UnlockDiagnosticVMOptions -verbose:gc -XX:+PrintCompilation -XX:+DebugNonSafepoints -jar socket_test $NUM_STREAMS $MODE > $FNAME.fifo &
   PID=$!
   cat $FNAME.fifo | tee $FNAME.out &
-  echo "Started proces $PID, waiting $PAUSE_BEFORE_PROFILING seconds before profiling"
+  echo Started proces $PID, waiting $PAUSE_BEFORE_PROFILING seconds before profiling
   sleep $PAUSE_BEFORE_PROFILING
-  echo "Storing process TOP output and starting profiling"
-  collectTopInfo
-  echo "Starting profiler"
-  # profiler/profiler.sh $PID > "$FNAME.profile"
-  echo "Done profiling, killing process $PID"
+  top $TOP_FLAGS $PID > $FNAME.top
+#  profiler/profiler.sh $PID > "$FNAME.profile"
   cleanup
 }
 cleanup() {
-  kill -KILL $PID > /dev/null
+  kill -KILL $PID
   rm -f $FNAME.fifo
 }
-
 trap "echo 'Terminating background processes...'; cleanup; exit 1" SIGINT SIGTERM EXIT
 for name in fs2 native loom
 do
